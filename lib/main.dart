546b514b499085856227753a2383ab528d2b2257
lucas joel <lucas.joel1628@gmail.com>
2025-07-22 11:47:02 -0300

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:utilidades/src/app/app_widget.dart';
 import 'package:utilidades/src/views/counter_cubit.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  /* runApp(AppWidget()); */
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(AppWidget()); 
 
   /*
     riverpod - é um gerenciador de estado flexível e poderoso.
@@ -49,7 +55,7 @@ void main() {
 
   */
 
-  runApp(myApp());
+  //runApp(myApp());
 }
 
 class myApp extends StatelessWidget{
--- lib/src/app/app_widget.dart
+++ lib/src/app/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:utilidades/src/app/app_routes.dart';
+import 'package:utilidades/src/views/firebase_login_view.dart';
 import 'package:utilidades/src/views/login_view.dart';
 
 class AppWidget extends StatelessWidget {
@@ -11,10 +12,11 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Utilidades",
-      initialRoute: "/home",
+      initialRoute: "/loginfirebase",
       /* home: AuthService.checkLogin ? const HomeView() : const LoginView(), */
         routes: {
         "/login": (context) => LoginView(),
+        "/loginfirebase" : (context) => const FirebaseLoginView(),
         ...generateRoutes(),
       },
     );
